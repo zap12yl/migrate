06533ebbb6b1eabae5400b3b9ce8d3c86e08e5a0
John Evans <lgastako@gmail.com>
2014-11-12 13:08:56 -0800

    Bump rev for packaging.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 if __name__ == "__main__":
     setup(name="migrate",
-          version="0.0.2",
+          version="0.0.3",
           description="Database Migration Assistant",
           author="John Evans",
           author_email="lgastako@gmail.com",
